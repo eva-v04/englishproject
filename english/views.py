5a8367067e36b2c2a699077db195ef2a5bed4131
Evanthia Violeti <sba2200006@uoa.gr>
2025-07-08 16:35:52 +0300

    added sign up

--- english/views.py
+++ english/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
+from .forms import SignUp
 
 # Create your views here.
 def homepage(request):
     return render(request, 'homepage.html')
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUp(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('homepage')
+    else:
+        form = SignUp()
+    return render(request, 'signup.html', {'form': form})
